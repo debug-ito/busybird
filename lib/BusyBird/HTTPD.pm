45ce23edbe10386b0d58cb15c029b8d0f0feaf6d
Toshio Ito <debug.ito@gmail.com>
2011-11-03 19:20:16 +0900

    Working on BusyBird::HTTPD module.   I had trouble sending image files (text files are OK),   because the image data were somehow encoded.   Maybe related to UTF8 stuff.   I figured out that I can send binary data correctly,   if I send it through POE::Filter::Stream, and if I   send HTTP headers and content separately. Weird.

--- lib/BusyBird/HTTPD.pm
+++ lib/BusyBird/HTTPD.pm
@@ -1,14 +1,23 @@
 package BusyBird::HTTPD;
 use strict;
+use warnings;
 
-use POE;
-use POE::Component::Server::HTTP;
+use POE qw(Component::Server::TCP Filter::HTTPD);
+## use POE::Component::Server::HTTP;
 use HTTP::Status;
+use HTTP::Response;
+use HTTP::Request;
 use File::MimeInfo;
 use IO::File;
 
+use Data::Dumper;
+
+$| = 1;
+
 my $self;
 my $PATH_STATIC_BASE = '/static/';
+
+sub RC_WAIT() { 0;} ## DUMMY
 
 sub init() {
     my ($class, $content_dir) = @_;
@@ -26,20 +35,107 @@ sub init() {
 
 sub start() {
     my ($class) = @_;
-    my $aliases = POE::Component::Server::HTTP->new(
+    if(!defined($self)) {
+        die 'Call init() before start()';
+    }
+    POE::Component::Server::TCP->new(
         Port => 8888,
-        ContentHandler => {
-            '/comet_sample' => \&_handlerCometSample,
-            '/' => \&_handlerIndex,
-            $PATH_STATIC_BASE => \&_handlerStaticContent,
+        ClientInputFilter  => "POE::Filter::HTTPD",
+        ClientOutputFilter => "POE::Filter::Stream",
+        ClientConnected => sub {
+            print STDERR "got a connection from $_[HEAP]{remote_ip}\n";
         },
-        Headers => { Server => 'My Server' },
-        );
-## HTTPDを終了するにはshutdownイベントを送る必要がある。SIGINTをトラップしてshutdownに変換？
+        ClientInput => sub {
+            my $client_input = $_[ARG0];
+            
+            ## return;
+            ## print STDERR ("INPUT------\n" . $client_input . "\n");
+            ## if($client_input) {
+            ##     return;
+            ## }
+            print STDERR ("---- Create response\n");
+            my $response = HTTP::Response->new();
+            $self->_handlerStaticContent($client_input, $response);
+            ## $_[HEAP]{client}->put('DATA');
+            ## $_[HEAP]{client}->put(pack('C*', 255,255,255,255,255,255,255,255,));
+            ## $_[HEAP]{client}->put("\r\n");
+            ## my $ret_data = $response->as_string("\r\n");
+            $response->header('Content-Length', length($response->content));
+            $_[HEAP]{client}->put('HTTP/1.1 ' . $response->status_line . "\r\n" . $response->headers_as_string("\r\n") . "\r\n");
+            $_[HEAP]{client}->put($response->content);
+            
+            ## ** Looks like combination of HTTP::Response and
+            ## ** PoCo::Server::TCP corrupts $response->content if
+            ## ** it's binary data such as an image file. So I decided
+            ## ** to use POE::Filter::Stream (no-op filter) and to
+            ## ** send HTTP response headers and content
+            ## ** separately. Because they are sent by separate
+            ## ** packets, it may be unefficient in terms of bandwidth
+            ## ** usage, though.
+            
+            return;
+            
+            ## my $ret_data = $response->content;
+            ## my $as_str = $response->as_string("\r\n");
+            ## 
+            ## print $as_str;
+            ## print "----------------\n";
+            ## $_[HEAP]{client}->put($as_str);
+            ## $_[HEAP]{client}->put("-----------------\n");
+            ## 
+            ## print $ret_data;
+            ## my @headers = (
+            ##     'HTTP/1.1 200 OK',
+            ##     'Content-Type: image/gif',
+            ##     'Content-Length: ' . length($ret_data),
+            ##     'Connection: Close',
+            ##     '',
+            ##     ''
+            ##     );
+            ## $_[HEAP]{client}->put(join("\r\n", @headers) . $ret_data);
+            ## ## $_[HEAP]{client}->put($ret_data);
+            ## ## $_[HEAP]{client}->put($response);
+        },
+  );
+##     my $aliases = POE::Component::Server::HTTP->new(
+##         Port => 8888,
+##         ContentHandler => {
+##             '/comet_sample' => \&_handlerCometSample,
+##             '/' => \&_handlerIndex,
+##             $PATH_STATIC_BASE => \&_handlerStaticContent,
+##         },
+##         Headers => { Server => 'My Server' },
+##         );
 
-## 新着件数などのサーバからのpush性の強いデータはcomet的にデータを送るといいかも
-## http://d.hatena.ne.jp/dayflower/20061116/1163663677
-## でもここのサンプル、実際動かしてみると二つ以上のコネクションを同時に張ってくれないような…
+## ** HTTPDを終了するにはshutdownイベントを送る必要がある。SIGINTをトラップしてshutdownに変換？
+## ** 新着件数などのサーバからのpush性の強いデータはcomet的にデータを送るといいかも
+## ** http://d.hatena.ne.jp/dayflower/20061116/1163663677
+## ** でもここのサンプル、実際動かしてみると二つ以上のコネクションを同時に張ってくれないような…
+}
+
+sub _handlerSample() {
+    my ($self, $client, $client_input) = @_;
+    my $response = HTTP::Response->new();
+    my $path = $self->{content_dir}."/small.gif";
+    my $mimetype = mimetype($path);
+    $mimetype = 'application/octet-stream' if !defined($mimetype);
+    my $file = IO::File->new();
+    if(!$file->open($path, "r")) {
+        &_setNotFound($response);
+        print STDERR ("3\n");
+        return $response;
+    }
+    my $filedata = '';
+    {
+        local $/ = undef;
+        $filedata = $file->getline();
+    }
+    $file->close();
+    $response->header('Content-Type', $mimetype);
+    $response->header('Content-Length', length($filedata));
+    $response->content_ref(\$filedata);
+    $response->code(200);
+    return $response;
 }
 
 sub _handlerIndex() {
@@ -67,9 +163,18 @@ sub _setNotFound() {
 }
 
 sub _handlerStaticContent() {
-    my ($request, $response) = @_;
+    my ($self, $request, $response) = @_;
     print STDERR ("URI: " . $request->uri . "\n");
-    if($request->uri !~ m|^https?://[^/]+$PATH_STATIC_BASE(.*)$|) {
+    my ($req_host, $req_path) = ('', '');
+    if($request->uri =~ m|^https?://([^/]+)(.+?)$|) {
+        $req_host = $1;
+        $req_path = $2;
+    }else {
+        $req_path = $request->uri;
+    }
+    $req_path = '/' . $req_path if $req_path !~ m|^/|;
+    
+    if($req_path !~ m|^$PATH_STATIC_BASE(.*)$|) {
         &_setNotFound($response);
         print STDERR ("1\n");
         return RC_OK;
@@ -91,7 +196,6 @@ sub _handlerStaticContent() {
         return RC_OK;
     }
     my $filedata = '';
-    $| = 1;
     {
         ## local $/ = undef;
         ## $filedata = $file->getline();
