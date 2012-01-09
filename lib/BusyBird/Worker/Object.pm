977453daf857e6ed79af9b48b26191741c1e807a
Toshio Ito <debug.ito@gmail.com>
2012-01-09 12:09:46 +0900

    Refactor BusyBird::Worker::Twitter to BusyBird::Worker::Object, and these are now subclass of BusyBird::Worker, not factory classes.   This makes it easy to extend their functionality more flexibly.

---
+++ lib/BusyBird/Worker/Object.pm
@@ -0,0 +1,90 @@
+package BusyBird::Worker::Object;
+use base ('BusyBird::Worker');
+
+use strict;
+use warnings;
+use POE qw(Filter::Reference);
+
+sub STATUS_OK { 0 };
+sub STATUS_NO_METHOD { 1 };
+sub STATUS_METHOD_DIES { 2 };
+
+
+sub _CONTEXT_LIST   { 0 };
+sub _CONTEXT_SCALAR { 1 };
+
+sub _makeOutputObject {
+    my ($status, $data) = @_;
+    return {status => $status, data => $data};
+}
+
+sub _getContextID {
+    my ($context_str) = @_;
+    return _CONTEXT_LIST if !defined($context_str);
+    $context_str = lc($context_str);
+    if($context_str eq 's' || $context_str eq 'scalar') {
+        return _CONTEXT_SCALAR;
+    }
+    return _CONTEXT_LIST;
+}
+
+sub new {
+    my ($class, $target_object) = @_;
+    my $self = $class->SUPER::new(
+        Program => sub {
+            POE::Kernel->stop();
+            my $input_str;
+            {
+                local $/ = undef;
+                $input_str = <STDIN>;
+            }
+            my $filter = POE::Filter::Reference->new();
+            my $command_objs = $filter->get([$input_str]);
+            
+            my $output_obj = [];
+            foreach my $command (@$command_objs) {
+                my ($method_name, $args_array, $context_str) = ($command->{method}, $command->{args}, $command->{context});
+                my $context = &_getContextID($context_str);
+                if(!$target_object->can($method_name)) {
+                    my $error_msg = sprintf ("ERROR: Method %s is undefined on %s.", $method_name, ref($target_object));
+                    print STDERR ("$error_msg\n");
+                    push(@$output_obj, &_makeOutputObject(STATUS_NO_METHOD, $error_msg));
+                    next;
+                }
+                my $ret;
+                eval {
+                    if($context == _CONTEXT_SCALAR) {
+                        my $return_val = $target_object->$method_name(@$args_array);
+                        $ret = &_makeOutputObject(STATUS_OK, $return_val);
+                    }else {
+                        my @return_vals = $target_object->$method_name(@$args_array);
+                        $ret = &_makeOutputObject(STATUS_OK, \@return_vals);
+                    }
+                };
+                if($@) {
+                    my $error_msg = ("ERROR: %s::%s: %s", ref($target_object), $method_name, $@);
+                    print STDERR ("$error_msg\n");
+                    push(@$output_obj, &_makeOutputObject(STATUS_METHOD_DIES, $error_msg));
+                    next;
+                }
+                push(@$output_obj, $ret);
+            }
+            my $serialized_chunks = $filter->put($output_obj);
+            foreach my $chunk (@$serialized_chunks) {
+                print $chunk;
+            }
+        },
+        StdinFilter  => POE::Filter::Reference->new(),
+        StdoutFilter => POE::Filter::Reference->new(),
+    );
+    $self->{target_object} = $target_object;
+    return $self;
+}
+
+sub getTargetObject {
+    my ($self) = @_;
+    return $self->{target_object};
+}
+
+1;
+
--- lib/BusyBird/Worker/Twitter.pm
+++ lib/BusyBird/Worker/Twitter.pm
@@ -1,58 +1,17 @@
 package BusyBird::Worker::Twitter;
+use base ('BusyBird::Worker::Object');
 
 use strict;
 use warnings;
-use POSIX qw(_exit);
-use POE qw(Filter::Reference);
 use Net::Twitter;
-use BusyBird::Worker;
-use Data::Dumper;
 
-sub create {
+sub new {
     my ($class, %net_twitter_params) = @_;
     $net_twitter_params{traits} ||= [qw(OAuth API::REST API::Lists)];
     $net_twitter_params{ssl}    ||= 1;
-    my $nt = Net::Twitter->new(%net_twitter_params); # Imported into "Program" closure
-    return BusyBird::Worker->new(
-        Program => sub {
-            POE::Kernel->stop();
-            my $input_str;
-            {
-                local $/ = undef;
-                $input_str = <STDIN>;
-            }
-            my $filter = POE::Filter::Reference->new();
-            my $command_objs = $filter->get([$input_str]);
-            ## print STDERR (Dumper($command_objs));
-            ## _exit(0);
-            
-            my $output_obj = [];
-            foreach my $command (@$command_objs) {
-                my ($method_name, $arg_hash_ref) = ($command->{method}, $command->{arg});
-                if(!$nt->can($method_name)) {
-                    printf STDERR ("Method %s is undefined on Net::Twitter.\n", $method_name);
-                    next;
-                }
-                my $ret;
-                eval {
-                    $ret = $nt->$method_name($arg_hash_ref);
-                };
-                if($@) {
-                    printf STDERR ("Error: Net::Twitter::%s: %s\n", $method_name, $@);
-                    next;
-                }
-                push(@$output_obj, $ret);
-            }
-            my $serialized_chunks = $filter->put($output_obj);
-            foreach my $chunk (@$serialized_chunks) {
-                print $chunk;
-            }
-        },
-        StdinFilter  => POE::Filter::Reference->new(),
-        StdoutFilter => POE::Filter::Reference->new(),
-    );
+    my $nt = Net::Twitter->new(%net_twitter_params);
+    return $class->SUPER::new($nt);
 }
-
 
 1;
 
