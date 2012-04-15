2036ed1122e51f079ad8ca50c5ad8354f3d77723
Toshio Ito <debug.ito@gmail.com>
2012-04-15 17:33:51 +0900

    Add Timer.pm, which is dedicated to timer function using AnyEvent.

---
+++ lib/BusyBird/Timer.pm
@@ -0,0 +1,40 @@
+package BusyBird::Timer;
+use base ('BusyBird::Object');
+use strict;
+use warnings;
+
+use AnyEvent;
+
+sub new {
+    my ($class, %params) = @_;
+    my $self = bless {
+        interval => undef,
+        callbacks => [],
+    }, $class;
+    $self->_setParam(\%params, 'interval', 120);
+    $self->start();
+    return $self;
+}
+
+sub start {
+    my ($self) = @_;
+    my $watcher;
+    $watcher = AnyEvent->timer(
+        after => $self->{interval},
+        cb => sub {
+            undef $watcher;
+            foreach my $callback (@{$self->{callbacks}}) {
+                $callback->();
+            }
+            $self->start();
+        }
+    );
+}
+
+sub addOnFire {
+    my ($self, $callback) = @_;
+    push(@{$self->{callbacks}}, $callback);
+}
+
+1;
+
