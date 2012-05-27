071e38f270449e4ee7022802203f283089033e3e
Toshio Ito <debug.ito@gmail.com>
2012-05-27 16:54:11 +0900

    busybird.pl: now config filename can be specified from command-line.

--- busybird.pl
+++ busybird.pl
@@ -11,16 +11,20 @@ use BusyBird::Input;
 use BusyBird::Output;
 use BusyBird::Timer;
 
-do "config.test.pl";
+my $OPT_THRESHOLD_OFFSET = 0;
+my $OPT_CONFIGFILE = 'config.test.pl';
+GetOptions(
+    't=s' => \$OPT_THRESHOLD_OFFSET,
+    'c=s' => \$OPT_CONFIGFILE,
+);
+
+
+do "$OPT_CONFIGFILE";
 if($@) {
     print STDERR ("Load config.test.pl error: $@\n");
     exit 1;
 }
 
-my $OPT_THRESHOLD_OFFSET = 0;
-GetOptions(
-    't=s' => \$OPT_THRESHOLD_OFFSET,
-);
 
 sub main {
     ## ** TODO: support more sophisticated format of threshold offset (other than just seconds).
