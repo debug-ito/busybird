dd5456bf3b0b40f29b136b18404edaf1b6177e29
Toshio Ito <debug.ito@gmail.com>
2012-05-27 17:26:31 +0900

    busybird.pl: Add initialization of ComponentManager. This loads saved statuses for Output and time files for Input.

--- busybird.pl
+++ busybird.pl
@@ -10,6 +10,8 @@ use AnyEvent;
 use BusyBird::Input;
 use BusyBird::Output;
 use BusyBird::Timer;
+use BusyBird::ComponentManager;
+
 
 my $OPT_THRESHOLD_OFFSET = 0;
 my $OPT_CONFIGFILE = 'config.test.pl';
@@ -19,17 +21,18 @@ GetOptions(
 );
 
 
+BusyBird::ComponentManager->init();
 do "$OPT_CONFIGFILE";
 if($@) {
     print STDERR ("Load config.test.pl error: $@\n");
     exit 1;
 }
 
-
 sub main {
     ## ** TODO: support more sophisticated format of threshold offset (other than just seconds).
     BusyBird::Input->setThresholdOffset(int($OPT_THRESHOLD_OFFSET));
     &configBusyBird($FindBin::Bin);
+    BusyBird::ComponentManager->initComponents();
     AnyEvent->condvar->recv();
 }
 
