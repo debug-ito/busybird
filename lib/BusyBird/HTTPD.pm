3dd1f11d705656acfc6bf93777fec93e8fffb75a
Toshio Ito <debug.ito@gmail.com>
2012-07-22 11:00:25 +0900

    HTTPD: use Text::MicroTemplate::Extended to handle templates.

--- lib/BusyBird/HTTPD.pm
+++ lib/BusyBird/HTTPD.pm
@@ -1,6 +1,7 @@
 package BusyBird::HTTPD;
 use strict;
 use warnings;
+use constant DEFAULT_DOCUMENT_ROOT => './resources/httpd';
 
 use Carp;
 use AnyEvent;
@@ -12,6 +13,7 @@ use BusyBird::Output;
 use Twiggy::Server;
 use Plack::Builder;
 use Plack::Request;
+use Text::MicroTemplate::Extended;
 
 
 my $g_httpd_self = undef;
@@ -21,6 +23,7 @@ sub init {
     $g_httpd_self = bless {
         backend => undef,
         request_points => {},
+        template_engine => undef,
     }, $class;
 }
 
@@ -36,9 +39,14 @@ sub start {
         host => $params{bind_address} || '127.0.0.1',
         port => $params{bind_port} || 8888,
     );
+    $params{document_root} ||= DEFAULT_DOCUMENT_ROOT;
+    $params{document_root} =~ s!/+$!!;
+    $self->{template_engine} = Text::MicroTemplate::Extended->new(
+        include_path => ["$params{document_root}/templates"]
+    );
     my $app = builder {
         enable "Plack::Middleware::ContentLength";
-        enable "Plack::Middleware::Static", path => qr{^/static/}, root => $params{document_root} || './resources/httpd/';
+        enable "Plack::Middleware::Static", path => qr{^/static/}, root => $params{document_root};
         $self->_createApp()
     };
     if(defined($params{customize})) {
@@ -123,79 +131,15 @@ sub addOutput {
     );
     $self->addRequestPoint(
         '/' . $output->getName() . '/index', sub {
-            my %S = (
+            my $page = $self->{template_engine}->render(
+                'output',
                 global_header_height => '50px',
                 global_side_height => '200px',
                 side_width => '150px',
                 optional_width => '100px',
                 profile_image_section_width => '50px',
+                name => $output->getName(),
             );
-            my $name = $output->getName();
-            my $page = <<"END";
-<html>
-  <head>
-    <title>$name - BusyBird</title>
-    <meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
-    <link rel="stylesheet" href="/static/style.css" type="text/css" media="screen" />
-    <style type="text/css"><!--
-
-div#global_header {
-    height: $S{global_header_height};
-}
-
-div#global_side {
-    top: $S{global_header_height};
-    width: $S{side_width};
-    height: $S{global_side_height};
-}
-
-div#side_container {
-    width: $S{side_width};
-    margin: $S{global_side_height} 0 0 0;
-}
-
-div#main_container {
-    margin: $S{global_header_height} $S{optional_width} 0 $S{side_width};
-}
-
-div#optional_container {
-    width: $S{optional_width};
-}
-
-div.status_profile_image {
-    width: $S{profile_image_section_width};
-}
-
-div.status_main {
-    margin: 0 0 0 $S{profile_image_section_width};
-}
-
-    --></style>
-    <script type="text/javascript" src="/static/jquery.js"></script>
-    <script type="text/javascript"><!--
-    function bbGetOutputName() {return "$name"}
---></script>
-    <script type="text/javascript" src="/static/main.js"></script>
-  </head>
-  <body>
-    <div id="global_header">
-    </div>
-    <div id="global_side">
-    </div>
-    <div id="side_container">
-    </div>
-    <div id="optional_container">
-    </div>
-    <div id="main_container">
-      <ul id="statuses">
-      </ul>
-      <div id="main_footer">
-        <button id="more_button" type="button" onclick="" >More...</button>
-      </div>
-    </div>
-  </body>
-</html>
-END
             return httpResSimple(200, \$page, 'text/html');
         }
     );
