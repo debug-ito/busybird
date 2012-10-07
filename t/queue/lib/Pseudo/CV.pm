695e477728383af6c1bfb317074e55810d8953fb
Toshio Ito <debug.ito@gmail.com>
2012-10-07 12:49:57 +0900

    t/queue/lib/Pseudo/CV: now send() method accepts parameters, which can be retrieved by recv() method. recv() now does not run the event loop when send() has been already called.

--- t/queue/lib/Pseudo/CV.pm
+++ t/queue/lib/Pseudo/CV.pm
@@ -7,14 +7,27 @@ sub new {
     my ($class) = @_;
     return bless {
         count => 0,
+        returned => [],
+        sent => 0,
     }, $class;
 }
 
 sub recv {
+    my $self = shift;
+    if($self->{sent}) {
+        return wantarray ? @{$self->{returned}} : $self->{returned}[0];
+    }
     EV::run;
+    return wantarray ? @{$self->{returned}} : $self->{returned}[0];
 }
 
 sub send {
+    my ($self, @param) = @_;
+    if($self->{sent}) {
+        return;
+    }
+    $self->{sent} = 1;
+    @{$self->{returned}} = @param;
     EV::break;
 }
 
