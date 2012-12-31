4320a6972124d63a3e0f7cfd1ce258e22850f15d
Ubuntu <ubuntu@ip-10-191-53-187.ec2.internal>
2012-12-31 01:55:10 +0000

    Add DateTime::Format and its tests.

---
+++ lib/App/BusyBird/DateTime/Format.pm
@@ -0,0 +1,111 @@
+package App::BusyBird::DateTime::Format;
+use strict;
+use warnings;
+use DateTime::Format::Strptime;
+use DateTime::Format::RFC3339;
+use Try::Tiny;
+
+our $preferred = 0;
+
+my %OPT_DEFAULT = (
+    locale => 'en_US',
+    on_error => 'undef',
+);
+
+my @FORMATS = (
+    DateTime::Format::Strptime->new(
+        %OPT_DEFAULT,
+        pattern => '%a %b %d %T %z %Y',
+    ),
+    DateTime::Format::Strptime->new(
+        %OPT_DEFAULT,
+        pattern => '%a, %d %b %Y %T %z',
+    ),
+    DateTime::Format::RFC3339->new(),
+);
+
+sub new {
+    my ($class) = @_;
+    return bless {}, $class;
+}
+
+sub parse_datetime {
+    my ($class_self, $string) = @_;
+    my $parsed;
+    foreach my $f (@FORMATS) {
+        $parsed = try {
+            $f->parse_datetime($string);
+        }catch {
+            undef;
+        };
+        last if defined($parsed);
+    }
+    return $parsed;
+}
+
+sub format_datetime {
+    my ($class_self, $datetime) = @_;
+    return $FORMATS[$preferred]->format_datetime($datetime);
+}
+
+1;
+
+=pod
+
+=head1 NAME
+
+App::BusyBird::DateTime::Format - DateTime::Format for App::BusyBird
+
+=head1 DESCRIPTION
+
+This class is the standard DateTime::Format in App::BusyBird.
+
+It can parse the following format.
+
+=over
+
+=item *
+
+'created_at' format of Twitter API.
+
+=item *
+
+'created_at' format of Twitter Search API v1.0.
+
+=item *
+
+RFC3339 format. See L<DateTime::Format::RFC3339>.
+
+=back
+
+It formats L<DateTime> object in 'created_at' format of Twitter API.
+
+
+=head1 CLASS METHODS
+
+=head2 $f = App::BusyBird::DateTime::Format->new()
+
+Creates a formatter.
+
+=head1 CLASS AND OBJECT METHODS
+
+The following methods can apply both to class and to an object.
+
+=head2 $datetime = $f->parse_datetime($string)
+
+Parse C<$string> to get L<DateTime> object.
+
+If given an improperly formatted string, this method returns C<undef>. It NEVER croaks.
+
+=head2 $string = $f->format_datetime($datetime)
+
+Format L<DateTime> object to a string.
+
+
+=head1 AUTHOR
+
+Toshio Ito 
+
+
+=cut
+
---
+++ t/DateTime_Format.t
@@ -0,0 +1,54 @@
+use strict;
+use warnings;
+use DateTime;
+use Test::More;
+
+BEGIN {
+    use_ok('App::BusyBird::DateTime::Format');
+}
+
+my $formatter = 'App::BusyBird::DateTime::Format';
+
+sub DT {
+    my ($year, $month, $day, $hour, $minute, $second, $time_zone) = @_;
+    return DateTime->new(
+        year => $year,
+        month => $month,
+        day => $day,
+        hour => $hour,
+        minute => $minute,
+        second => $second,
+        time_zone => $time_zone,
+    );
+}
+
+sub checkParse {
+    my ($str, $exp_dt) = @_;
+    my $got_dt = $formatter->parse_datetime($str);
+    if(defined($exp_dt)) {
+        cmp_ok(DateTime->compare($got_dt, $exp_dt), '==', 0, "parsed to $exp_dt");
+    }else {
+        ok(!defined($got_dt), "expect to fail parsing");
+    }
+}
+
+sub checkFormat {
+    my ($dt, $exp_str) = @_;
+    my $got_str = $formatter->format_datetime($dt);
+    is($got_str, $exp_str, "format to $exp_str");
+}
+
+checkParse "Fri Jul 16 16:58:46 +0200 2010", DT qw(2010 7 16 16 58 46 +0200);
+checkParse "Mon Dec 03 00:01:23 +0900 2012", DT qw(2012 12 3 0 1 23 +0900);
+checkParse "some text here. Wed  Feb  29 23:34:06 +0900  2012", undef;
+checkParse "Thu, 06 Oct 2011 19:36:17 +0000", DT qw(2011 10 6 19 36 17 +0000);
+checkParse "Sun, 23 Oct 2011 19:03:12 -0500", DT qw(2011 10 23 19 3 12 -0500);
+checkParse "2012-09-23T11:00:10+0800", undef;
+checkParse "2012-09-23T11:00:10+08:00", DT qw(2012 9 23 11 0 10 +0800);
+checkParse "2011/07/12 04:02:00-10:30", undef;
+
+checkFormat DT(qw(2010 8 22 3 34 0 +0900)), "Sun Aug 22 03:34:00 +0900 2010";
+checkFormat DT(qw(2012 1 1 15 8 45 +2000)), "Sun Jan 01 15:08:45 +2000 2012";
+
+
+done_testing();
