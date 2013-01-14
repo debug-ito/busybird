9464f34d4761b569a50a1255d2a5bdf835dd62fd
Toshio Ito <debug.ito@gmail.com>
2013-01-14 22:10:16 +0900

    Timeline.pm: (probably) complete the spec of Timeline.

--- lib/App/BusyBird/Timeline.pm
+++ lib/App/BusyBird/Timeline.pm
@@ -1,4 +1,6 @@
 package App::BusyBird::Timeline;
+use strict;
+use warnings;
 
 1;
 
@@ -12,17 +14,33 @@ App::BusyBird::Timeline - a timeline object in BusyBird
 
 =head1 OBJECT METHODS
 
+=head2 $name = $timeline->name();
+
 =head2 $timeline->add_statuses($arrayref_of_statuses, [$callback->($added_num, $error)])
 
 =head2 $timeline->confirm([$callback->($confirmed_num, $error)])
 
-=head2 $timeline->get_statuses(%args)
+=head2 $timeline->get_statuses(%options, $callback->($arrayref_of_statuses, $error))
+
+=over
+
+=item C<max_id>
+
+=item C<count>
+
+=item C<confirm_state>
+
+=back
 
 =head2 $timeline->contains($arrayref_of_ids, $callback->($contained_ids, $not_contained_ids, $error))
 
-=head2 $timeline->selector()
+=head2 %unconfirmed_counts = $timeline->unconfirmed_counts()
 
-=head2 $storage = $timeline->get_status_storage()
+=head2 $selector = $timeline->updates_selector()
+
+Where should I write the specification of updates?
+
+=head2 $storage = $timeline->status_storage()
 
 =head1 AUTHOR
 
