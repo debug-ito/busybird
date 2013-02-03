6254cbe2eaf3e718c205b041ab115e0fb3796425
Toshio Ito <debug.ito@gmail.com>
2013-02-03 17:55:29 +0900

    Update Filters.pm POD.

--- lib/App/BusyBird/Filters.pm
+++ lib/App/BusyBird/Filters.pm
@@ -12,13 +12,21 @@ App::BusyBird::Filters - some status filters you might find useful
 
 =head1 EXPORTABLE FUNCTIONS
 
-=head2 $filter = filter_exec(@args)
+=head2 ($filter, 0) = filter_exec(@args)
 
 Synchronous filter.
 
-=head2 $filter_async = filter_exec_anyevent(@args)
+=head2 ($filter, 1) = filter_exec_anyevent(@args)
 
-=head2 $filter = filter_user_levels(%level_spec)
+=head2 ($filter, 0) = filter_user_levels(%level_spec)
+
+=head2 ($filter, 1) = filter_only_new($timeline)
+
+Be sure C<weaken> the C<$timeline>!!
+
+=head2 ($filter, 0) = filter_distribute_to(@timelines)
+
+Be sure C<weakn> the C<@timelines>!!
 
 =cut
 
