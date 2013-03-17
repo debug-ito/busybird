24d6d8bc3788e9a64bb7a58655debf4cfd91bce7
Toshio Ito <debug.ito@gmail.com>
2013-03-17 15:23:19 +0900

    BusyBird.pm: add $LOCAL_SHAREDIR flag to indicate local 'share' directory is used for testing.

--- lib/BusyBird.pm
+++ lib/BusyBird.pm
@@ -3,9 +3,10 @@ use strict;
 use warnings;
 use File::ShareDir qw(dist_dir);
 
+our $LOCAL_SHAREDIR = 0;
 
 sub sharedir {
-    return dist_dir(__PACKAGE__);
+    return $LOCAL_SHAREDIR ? './share' : dist_dir(__PACKAGE__);
 }
 
 our $VERSION = '0.01';
