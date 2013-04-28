35269dde65f2662725fe1213c9a046801945c502
Toshio Ito <debug.ito@gmail.com>
2013-04-28 11:51:19 +0900

    busybird.js: implement setDisplayByThreshold. Pass the basic tests.

--- share/www/static/busybird.js
+++ share/www/static/busybird.js
@@ -92,6 +92,30 @@ bb.distanceRanges = function (a_top, a_range, b_top, b_range) {
     return (signed_dist > 0 ? signed_dist : 0);
 };
 
+bb.slideToggleElements = function($elements, duration, step_func) {
+    var deferred = Q.defer();
+    if(!step_func) {
+        step_func = function(now, fx) {};
+    }
+    $elements.animate(
+        { "height": "toggle",
+          "marginTop": "toggle",
+          "marginBottom": "toggle",
+          "paddingTop": "toggle",
+          "paddingBottom": "toggle"
+        },
+        {
+            duration: duration,
+            step: step_func
+        }
+    ).promise().done(function() {
+        deferred.resolve();
+    }).fail(function() {
+        deferred.reject("Animation somehow failed.");
+    });
+    return deferred.promise;
+};
+
 bb.Spinner = function(sel_target) {
     this.sel_target = sel_target;
     this.spin_count = 0;
@@ -155,78 +179,155 @@ bb.MessageBanner.prototype = {
 bb.StatusContainer = $.extend(function(sel_container) {
     this.sel_container = sel_container;
 }, {
+    ANIMATE_STATUS_MAX_NUM: 15,
+    ANIMATE_STATUS_DURATION: 400,
+    _formatHiddenStatusesHeader : function (invisible_num) {
+        var plural = invisible_num > 1 ? "es" : "";
+        return '<li class="bb-hidden-statuses-header"><span class="bb-hidden-statuses-num">'+ invisible_num +'</span> status'+plural+' hidden here.</li>';
+    },
+    _updateHiddenStatusesHeaders: function($statuses, hidden_header_list, window_adjuster) {
+        var selfclass = this;
+        if(!defined(window_adjuster)) window_adjuster = function() {};
+        $statuses.filter(".bb-hidden-statuses-header").remove();
+        window_adjuster();
+        return bb.blockEach(hidden_header_list, 40, function(header_block) {
+            $.each(header_block, function(i, header_entry) {
+                if(defined(header_entry.$followed_by)) {
+                    console.log("intermediate: " + header_entry.entries.length);
+                    header_entry.$followed_by.before(selfclass._formatHiddenStatusesHeader(header_entry.entries.length));
+                }else {
+                    $statuses.filter('.bb-status').last().after(selfclass._formatHiddenStatusesHeader(header_entry.entries.length));
+                }
+            });
+            window_adjuster();
+        });
+    },
+    _createWindowAdjuster: function(dom_anchor_elem) {
+        var $anchor_elem;
+        var relative_position_of_anchor;
+        if(!defined(dom_anchor_elem)) {
+            return function() {};
+        }
+        $anchor_elem = $(dom_anchor_elem);
+        relative_position_of_anchor = $anchor_elem.offset().top - $(window).scrollTop();
+        return function() {
+            $(window).scrollTop($anchor_elem.offset().top - relative_position_of_anchor);
+        };
+    },
     _scanStatusesForDisplayActions: function($statuses, threshold_level, enable_animation, cursor_index) {
+        var selfclass = this;
         var ACTION_STAY_VISIBLE = 0;
         var ACTION_STAY_INVISIBLE = 1;
-        var ACTION_GET_VISIBLE = 2;
-        var ACTION_GET_INVISIBLE = 3;
+        var ACTION_BECOME_VISIBLE = 2;
+        var ACTION_BECOME_INVISIBLE = 3;
         var final_result = { // ** return this struct from the promise
             hidden_header_list: [],
-            doms_animate_show: [],
-            doms_animate_hide: [],
-            doms_immediate_show: [],
-            doms_immediate_hide: [],
+            doms_animate_toggle: [],
+            doms_immediate_toggle: [],
+            dom_anchor_elem: null,
         };
         var metrics_list = [];
         var next_seq_invisible_entries = [];
-        var prev_pos;
-        var win_dim;
+        var prev_pos = 0;
+        var win_dim = {"top": $(window).scrollTop(), "range": $(window).height()};
         if(!cursor_index) cursor_index = 0;
-        if(enable_animation) {
-            win_dim = {"top": $(window).scrollTop(), "range": $(window).height()};
-        }
         return bb.blockEach($statuses.filter(".bb-status").get(), 150, function(status_block, block_start_index) {
             $.each(status_block, function(index_in_block, cur_entry) {
                 var cur_index = block_start_index + index_in_block;
                 var $cur_entry = $(cur_entry);
                 var entry_level = $cur_entry.data('bb-status-level');
                 var cur_is_visible = ($cur_entry.css('display') !== 'none');
-                var cur_pos;
+                var cur_pos = (cur_is_visible ? $cur_entry.offset().top : prev_pos);
                 var metric = {
                     status_entry: cur_entry,
                     action: null,
                     win_dist: 0,
                     cursor_index_dist: 0
                 };
-                if(entry_level <= threshold_level) {
-                    metric.action = (cur_is_visible ? ACTION_STAY_VISIBLE : ACTION_GET_VISIBLE);
+                if(entry_level >= threshold_level) {
+                    metric.action = (cur_is_visible ? ACTION_STAY_VISIBLE : ACTION_BECOME_VISIBLE);
                     if(next_seq_invisible_entries.length > 0) {
                         final_result.hidden_header_list.push({'$followed_by': $cur_entry, 'entries': next_seq_invisible_entries});
                         next_seq_invisible_entries = [];
                     }
                 }else {
-                    metric.action = (cur_is_visible ? ACTION_GET_INVISIBLE : ACTION_STAY_INVISIBLE);
+                    metric.action = (cur_is_visible ? ACTION_BECOME_INVISIBLE : ACTION_STAY_INVISIBLE);
                     next_seq_invisible_entries.push($cur_entry);
                 }
-                if(enable_animation) {
-                    cur_pos = (cur_is_visible ? $cur_entry.offset().top : prev_pos);
-                    metric.win_dist = bb.distanceRanges(win_dim.top, win_dim.range, cur_pos, $cur_entry.height());
-                    metric.cursor_index_dist = Math.abs(cur_index - cursor_index);
-                    prev_pos = cur_pos;
-                }
+                metric.win_dist = bb.distanceRanges(win_dim.top, win_dim.range, cur_pos, $cur_entry.height());
+                metric.cursor_index_dist = Math.abs(cur_index - cursor_index);
+                prev_pos = cur_pos;
                 metrics_list.push(metric);
             });
         }).then(function() {
+            var animate_count_max = enable_animation ? selfclass.ANIMATE_STATUS_MAX_NUM : 0;
+            var animate_count = 0;
             if(next_seq_invisible_entries.length > 0) {
                 final_result.hidden_header_list.push({'$followed_by': null, 'entries': next_seq_invisible_entries});
             }
-            // このへんから。
+            metrics_list = metrics_list.sort(function (a, b) {
+                if(a.win_dist !== b.win_dist) {
+                    return a.win_dist - b.win_dist;
+                }
+                return a.cursor_index_dist - b.cursor_index_dist;
+            });
+            $.each(metrics_list, function(metrics_index, metric) {
+                var target_container;
+                if(final_result.dom_anchor_elem === null && metric.action === ACTION_STAY_VISIBLE) {
+                    final_result.dom_anchor_elem = metric.status_entry;
+                }
+                if(metric.action === ACTION_STAY_VISIBLE || metric.action === ACTION_STAY_INVISIBLE) {
+                    return true;
+                }
+                if(animate_count < animate_count_max) {
+                    animate_count++;
+                    target_container = final_result.doms_animate_toggle;
+                }else {
+                    target_container = final_result.doms_immediate_toggle;
+                }
+                target_container.push(metric.status_entry);
+            });
+            return final_result;
         });
     },
     setDisplayByThreshold: function(args) {
-        // @params: args.$statuses, args.threshold, args.enable_animation, args.enable_window_adjust
+        // @params: args.$statuses, args.threshold, args.enable_animation, args.enable_window_adjust, args.cursor_index
         // @returns: promise for completion event.
+        var selfclass = this;
         return Q.fcall(function() {
-            var $statuses, threshold;
             if(!defined(args.$statuses)) {
                 throw "$statuses param is mandatory";
             }
             if(!defined(args.threshold)) {
                 throw "threshold param is mandatory";
             }
-            $statuses = args.$statuses;
-            threshold = args.threshold;
-            
+            return selfclass._scanStatusesForDisplayActions(args.$statuses, args.threshold, args.enable_animation, args.cursor_index);
+        }).then(function(action_description) {
+            var window_adjuster, promise_hidden_statuses, promise_animation, promise_immeidate;
+            if(args.enable_window_adjust) {
+                window_adjuster = selfclass._createWindowAdjuster(action_description.dom_anchor_elem);
+            }else {
+                window_adjuster = function() {};
+            }
+            if(action_description.doms_animate_toggle.length > 0) {
+                promise_animation = bb.slideToggleElements(
+                    $(action_descritption.doms_animate_toggle), selfclass.ANIMATE_STATUS_DURATION,
+                    function(now, fx) {
+                        if(fx.prop !== "height") return;
+                        window_adjuster();
+                    }
+                );
+            }else {
+                promise_animation = Q.fcall(function() { });
+            }
+            promise_hidden_statuses = selfclass._updateHiddenStatusesHeaders(args.$statuses,
+                                                                             action_description.hidden_header_list,
+                                                                             window_adjuster);
+            promise_immediate = bb.blockEach(action_description.doms_immediate_toggle, 100, function(status_block) {
+                $(status_block).toggle();
+                window_adjuster();
+            });
+            return Q.all([promise_hidden_statuses, promise_animation, promise_immediate]);
         });
     },
     loadStatuses: function(args) {
@@ -234,10 +335,6 @@ bb.StatusContainer = $.extend(function(sel_container) {
     }
 });
 bb.StatusContainer.prototype = {
-    _formatHiddenStatus : function (invisible_num) {
-        var plural = invisible_num > 1 ? "es" : "";
-        return '<li class="bb-hidden-statuses-header"><span class="bb-hidden-statuses-num">'+ invisible_num +'</span> status'+plural+' hidden here.</li>';
-    },
     appendStatuses: function($added_statuses) {
         $(this.sel_container).append($added_statuses);
     },
