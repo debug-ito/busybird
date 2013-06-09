865a5afccd3ef213de7df12847550e2d3acc37e8
Toshio Ito <debug.ito@gmail.com>
2013-06-09 10:36:20 +0900

    Convert to Module::Build.

--- lib/BusyBird.pm
+++ lib/BusyBird.pm
@@ -1,8 +1,32 @@
 package BusyBird;
 use strict;
 use warnings;
-use BusyBird::Version;
-our $VERSION = $BusyBired::Version::VERSION;
 
+use BusyBird::Version; our $VERSION = $BusyBird::Version::VERSION;
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+BusyBird - a multi-level Web-based timeline viewer
+
+=head1 AUTHOR
+
+Toshio Ito C<< <toshioito [at] cpan.org> >>
+
+=head1 LICENSE
+
+Copyright 2013 Toshio Ito.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
