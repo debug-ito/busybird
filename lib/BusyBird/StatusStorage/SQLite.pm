cc81a039906fad4968378eed53b6c0989fdda7de
Toshio Ito <debug.ito@gmail.com>
2013-06-19 22:36:14 +0900

    Implementing StatusStorage::SQLite...

--- lib/BusyBird/StatusStorage/SQLite.pm
+++ lib/BusyBird/StatusStorage/SQLite.pm
@@ -4,6 +4,155 @@ use warnings;
 use base ("BusyBird::StatusStorage");
 use BusyBird::Version;
 our $VERSION = $BusyBird::Version::VERSION;
+use DBI;
+use Carp;
+use Try::Tiny;
+use SQL::Maker;
+use BusyBird::DateTime::Format;
+use JSON;
+no autovivification;
+
+my $UNDEF_TIMESTAMP = '9999-99-99T99:99:99';
+
+sub new {
+    my ($class, %args) = @_;
+    my $self = bless {
+        dbi_source => undef,
+        maker => SQL::Maker->new(driver => 'SQLite'),
+    }, $class;
+    croak "path parameter is mandatory" if not defined $args{path};
+    $self->{dbi_source} = "dbi:SQLite:dbname=$args{path}";
+    $self->_create_tables();
+    return $self;
+}
+
+sub _get_dbh {
+    my ($self, $dbi_source, $dbi_username, $dbi_password, $attr) = @_;
+    return DBI->connect($dbi_source, $dbi_username, $dbi_password, $attr);
+}
+
+sub _get_my_dbh {
+    my ($self) = @_;
+    return $self->_get_dbh($self->{dbi_source}, "", "", {
+        RaiseError => 1, PrintError => 0, AutoCommit => 1
+    });
+}
+
+sub _create_tables {
+    my ($self) = @_;
+    my $dbh = $self->_get_my_dbh();
+    $dbh->do(<<EOD);
+CREATE TABLE IF NOT EXISTS statuses (
+  id TEXT PRIMARY KEY UNIQUE NOT NULL,
+  timeline_id INTEGER NOT NULL,
+  level INTEGER NOT NULL,
+  utc_acked_at TEXT NOT NULL,
+  utc_created_at TEXT NOT NULL,
+  timezone_acked_at TEXT NOT NULL,
+  timezone_created_at TEXT NOT NULL,
+  content TEXT NOT NULL
+)
+EOD
+    $dbh->do(<<EOD);
+CREATE TABLE IF NOT EXISTS timelines (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  name TEXT UNIQUE NOT NULL,
+)
+EOD
+}
+
+sub put_statuses {
+    my ($self, %args) = @_;
+    my $timeline = $args{timeline};
+    croak "timeline parameter is mandatory" if not defined $timeline;
+    my $mode = $args{mode};
+    croak "mode parameter is mandatory" if not defined $mode;
+    if($mode ne 'insert' && $mode ne 'update' && $mode ne 'upsert') {
+        croak "mode must be either insert, update or upsert";
+    }
+    my $statuses = $args{statuses};
+    croak "statuses parameter is mandatory" if not defined $statuses;
+    if(ref($statuses) ne 'HASH' && ref($statuses) ne 'ARRAY') {
+        croak "statuses parameter must be either a status object or an array-ref of statuses";
+    }
+    if(ref($statuses) eq 'HASH') {
+        $statuses = [$statuses];
+    }
+    my $callback = $args{callback} || sub {};
+    my $dbh;
+    try {
+        $dbh = $self->_get_my_dbh();
+        $dbh->begin_work();
+        my $timeline_id = $self->_get_timeline_id($dbh, $timeline);
+        my $sth;
+        my $total_count = 0;
+        foreach my $status (@$statuses) {
+            my $record = _to_status_record($timeline_id, $status);
+            if($mode eq 'update') {
+                my ($sql, @bind) = $self->{maker}->update('statuses', $record, [
+                    'timeline_id' => $timeline_id, id => $status->{id}
+                ]);
+                if(!$sth) {
+                    $sth = $dbh->prepare($sql);
+                }
+                my $count = $sth->execute(@bind);
+                if($count > 0) {
+                    $total_count += $count;
+                }
+            }elsif($mode eq 'insert') {
+                
+            }else {
+                ## upsert
+                
+            }
+        }
+        $dbh->commit();
+    } catch {
+        my $e = shift;
+        if($dbh) {
+            $dbh->rollback();
+        }
+        $callback->($e);
+    };
+}
+
+sub _get_timeline_id {
+    my ($self, $dbh, $timeline_name) = @_;
+    my ($sql, @bind) = $self->{maker}->select('timelines', ['id'], ['name' => $timeline_name]);
+    my $record = $dbh->selectrow_arrayref($sql, undef, @bind);
+    if(!defined($record)) {
+        croak "No timeline named '$timeline_name'";
+    }
+    return $record->[0];
+}
+
+sub _to_status_record {
+    my ($timeline_id, $status) = @_;
+    croak "status ID must be set" if not defined $status->{id};
+    croak "timeline_id must be defined" if not defined $timeline_id;
+    my $record = {
+        id => $status->{id},
+        timeline_id => $timeline_id,
+        level => $status->{busybird}{level} || 0,
+    };
+    ($record->{utc_acked_at}, $record->{timezone_acked_at}) = _extract_utc_timestamp_and_timezone($status->{busybird}{acked_at});
+    ($record->{utc_created_at}, $record->{timezone_created_at}) = _extract_utc_timestamp_and_timezone($status->{created_at});
+    $record->{content} = encode_json($status);
+    return $record;
+}
+
+sub _extract_utc_timestamp_and_timezone {
+    my ($timestamp_str) = @_;
+    if(!defined($timestamp_str) || $timestamp_str eq '') {
+        return ($UNDEF_TIMESTAMP, 'UTC');
+    }
+    my $datetime = BusyBird::DateTime::Format->parse_datetime($timestamp_str);
+    croak "Invalid datetime format: $timestamp_str" if not defined $datetime;
+    my $timezone_name = $datetime->time_zone->name;
+    $datetime->set_time_zone('UTC');
+    my $utc_timestamp = $datetime->strftime('%Y-%m-%dT%H:%M:%S');
+    return ($utc_timestamp, $timezone_name);
+}
 
 1;
 
