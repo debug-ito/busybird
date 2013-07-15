65bc5360ca8a4d20f343d005a70c972d2b2206b8
Toshio Ito <debug.ito@gmail.com>
2013-07-15 17:34:53 +0900

    timeline_list.js: add bb.UnackedCountsPoller class.

--- share/www/static/timeline_list.js
+++ share/www/static/timeline_list.js
@@ -68,3 +68,61 @@ bb.UnackedCountsRenderer = (function() {
     return selfclass;
 })();
 
+bb.UnackedCountsPoller = (function() {
+    var selfclass = function(args) {
+        // @params: args.apiBase = ""
+        var self = this;
+        self.api_base = args.apiBase || "";
+        self.level = "total"; // ** for now, it's constant
+        self.polled_timelines = {};
+        self.poller = null;
+    };
+    selfclass.prototype = {
+        addTimeline: function(args) {
+            // @params: args.timelineName, args.callback(unacked_counts), args.initialUnackedCounts = {total: 0}
+            if(!defined(args.timelineName)) {
+                throw "timelineName param is mandatory";
+            }
+            if(!defined(args.callback)) {
+                throw "callback param is mandatory";
+            }
+            this.polled_timelines[args.timelineName] = {
+                callback: args.callback,
+                unacked_counts: args.initialUnackedCounts || {"total": 0}
+            };
+        },
+        _makeQuery: function() {
+            var self = this;
+            var query_obj = { "level": self.level };
+            var timeline_count = 0;
+            $.each(self.polled_timelines, function(name, polled_timeline) {
+                timeline_count++;
+                query_obj["tl_" + name] = polled_timeline.unacked_counts[self.level] || 0;
+            });
+            if(timeline_count === 0) {
+                throw "Cannot create valid query because there is no timeline added for polling.";
+            }
+            return query_obj;
+        },
+        start: function() {
+            var self = this;
+            self.poller = new bb.EventPoller({
+                url: self.api_base + "/updates/unacked_counts.json",
+                initialQuery: self._makeQuery(),
+                onResponse: function(response_data) {
+                    if(defined(response_data.error)) {
+                        return Q.reject("API error: " + response_data.error);
+                    }
+                    $.each(response_data.unacked_counts, function(timeline_name, got_counts) {
+                        var polled_timeline = self.polled_timelines[timeline_name];
+                        polled_timeline.unacked_counts = got_counts;
+                        polled_timeline.callback(got_counts);
+                    });
+                    return self._makeQuery();
+                }
+            });
+            self.poller.start();
+        }
+    };
+    return selfclass;
+})();
