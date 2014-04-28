aa512fdd46479cc845c75ec7092e4f26032e3139
Toshio Ito <debug.ito@gmail.com>
2014-04-28 19:15:14 +0900

    timeline.js: unacked counts pairs in status summary now have "white-space: nowrap" style so that the pair is never split.

--- share/www/static/timeline.js
+++ share/www/static/timeline.js
@@ -563,9 +563,11 @@ bb.StatusesSummary.prototype = {
         var accordion_body_id = "bb-summary-body-level" + entry.level;
         var $heading = $('<div class="accordion-heading">'
                        + '<a class="accordion-toggle" data-toggle="collapse" href="#'+ accordion_body_id +'">'
-                       + 'Lv. <span class="bb-summary-level">' + entry.level + '</span>&nbsp;&nbsp;'
+                       + 'Lv. <span class="bb-summary-level">' + entry.level + '</span> &nbsp;&nbsp;'
+                       + '<span class="bb-summary-count-pair">'
                        + '<span class="badge badge-info bb-summary-count-above-level">' + count_above + '</span> '
                        + (entry.count === count_above ? '' : '<span class="badge bb-summary-count-this-level">+' + entry.count + '</span>')
+                       + '</span>'
                        + '</a></div>');
         var $body = $('<div id="'+ accordion_body_id +'" class="accordion-body collapse"></div>')
                      .append($('<div class="accordion-inner"></div>').append(self._renderPerUserList(entry.per_user)));
